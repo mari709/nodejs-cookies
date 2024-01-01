1b26a37efb7dca9a056d793606677f5a3cdded96
Marina Mansueto <mansumdp@gmail.com>
2024-01-01 17:34:17 -0300

    estableciendo y asegurando cookies

--- server.js
+++ server.js
@@ -7,5 +7,15 @@ app.get('/', (req,res) => {
     res.send('hello world');
 })
 
+app.get('/setcookie', (req, res) => {
+    res.cookie('name cookie', 'cookie-valor', {
+        maxAge: 10000,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict' // lax
+        //expires: new Date("2024-03-31"),
+    })
+    res.send('hello cookie');
+})
 app.listen(3000);
 console.log(`server running on http://localhost:3000`);
